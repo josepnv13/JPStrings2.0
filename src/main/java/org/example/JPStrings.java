e5442c308a1021b4ab6c2a85d4ec656272c41953
CARLOS <carlos@example.com>
2023-03-01 16:45:27 +0100

    pololo

--- src/main/java/org/example/JPStrings.java
+++ src/main/java/org/example/JPStrings.java
@@ -1,2 +1,43 @@
-package org.example;public class JPStrings {
+package org.example;
+
+import java.util.Random;
+
+public class JPStrings {
+    /**
+     * Pone la primera letra en mayuscula y el resto en minuscula
+     * @param text
+     * @return
+     */
+    public static String capitalitzar(String text){
+
+       return text.substring(0,1).toUpperCase() + text.substring(1).toLowerCase();
+    }
+
+    /**
+     * pinta cada letra de un color...
+     *
+     * @param text
+     * @return
+     */
+
+    public static String colorear(String text){
+        Random random = new Random();
+        String resultat = "";
+        for (int i = 0; i < text.length(); i++) {
+            resultat += "\033[3" + (random.nextInt(8)) + "m" + text.charAt(i);
+        }
+        return resultat +"\033[0m";
+    }/**
+     * Pone espacios a la izquierda de las palabras...
+     *
+     * @param text
+     * @return
+     */
+
+    public static String leftPad(String str, int width) {
+
+        // si vull tindre 6 espais
+
+        return " ".repeat(width-str.length()) + str;
+    }
 }
--- src/test/java/JPStringsTests.java
+++ src/test/java/JPStringsTests.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class JPStringsTests {
+import org.example.JPStrings;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JPStringsTests {
+
+    @Test
+    void testCapitalitzar(){
+        JPStrings jpStrings = new JPStrings();
+
+        assertEquals("Gerard", JPStrings.capitalitzar("gerard"));
+        assertEquals("Gerard", JPStrings.capitalitzar("GERard"));
+        assertEquals("Gerard", JPStrings.capitalitzar("gerARD"));
+        assertEquals("Gerard", JPStrings.capitalitzar("Grard"));
+
+        assertEquals("Gerard", JPStrings.capitalitzar("gerard"));
+        assertEquals("Gerard", JPStrings.capitalitzar("GERard"));
+        assertEquals("Gerard", JPStrings.capitalitzar("gerARD"));
+
+
+
+        assertEquals("\033[31mg\033[32me", JPStrings.colorear("ge"));
+
+    }
+
+    @Test
+    void testLeftPad() {
+        assertEquals("    casa", JPStrings.leftPad("casa", 8));
+        assertEquals("      no", JPStrings.leftPad("no", 8));
+    }
+
 }
